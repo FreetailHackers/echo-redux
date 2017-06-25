1706833be3cfeaaf6d2191580437cb19fc5ff175
Thomas Gaubert <ThomasGaubert@users.noreply.github.com>
2017-06-25 02:25:10 -0700

    Android (#2)
    * Basic structure of Android app
    
    * SMS --> TTS pipeline
    
    * Update android app
    
    * more changes
    
    * more

--- android/app/src/main/java/com/hacktx/echoredux/MainActivity.java
+++ android/app/src/main/java/com/hacktx/echoredux/MainActivity.java
@@ -23,16 +23,21 @@ import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
-import com.gtranslate.Language;
-import com.gtranslate.Translator;
 
 import net.glxn.qrgen.android.QRCode;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -91,30 +96,26 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            // TODO: Tell server that we've received the message via SMS
+            sendStatusToServer("Receieved SMS message", 0, false, 5);
 
-            // TODO: Generate QR code
             Log.i(TAG, "Generating QR code...");
             String message = intent.getStringExtra("text");
             QRCode qrCode = QRCode.from(message).withSize(1000, 1000);
             Bitmap bitmap = qrCode.bitmap();
             ((ImageView) findViewById(R.id.qrCode)).setImageBitmap(bitmap);
 
-            // TODO: Tell server that we've generated the QR code
+            sendStatusToServer("Generated QR code", 1, false, 5);
 
-            // TODO: Save the image to disk
             saveToInternalStorage(bitmap);
 
-            // TODO: Tell server that we've saved the QR code
+            sendStatusToServer("Persisting QR code", 2, false, 5);
 
-            // TODO: Read the image from disk
             ContextWrapper cw = new ContextWrapper(getApplicationContext());
             File directory = cw.getDir("dir", Context.MODE_PRIVATE);
             Bitmap bitmap2 = loadImageFromStorage(directory.toString());
 
-            // TODO: Tell server that we've read the QR code
+            sendStatusToServer("Retrieved QR code", 3, false, 5);
 
-            // TODO: Parse the QR code
             Log.i("TAG", "Parsing QR code...");
 
             int width = bitmap2.getWidth(), height = bitmap2.getHeight();
@@ -134,25 +135,65 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (result == null) {
+                sendStatusToServer("Unable to parse QR code!", -1, true, 0);
                 throw new RuntimeException("Unable to parse QR code!");
             }
 
             final String textToTranslate = result.getText();
 
-            // TODO: Tell server that we've parsed the QR code
+            sendStatusToServer("Parsed QR code", 4, false, 5);
 
-            // TODO: Use Google Translate to translate back to English
             new Thread(new Runnable() {
                 public void run() {
-                    // Translator translate = Translator.getInstance();
-                    // String translatedText = translate.translate(textToTranslate, Language.ENGLISH, Language.ENGLISH);
-
-                    // TODO: Tell server that we've translated back to English
-
                     speak(textToTranslate);
                 }
             }).start();
         }
+    }
+
+    private void sendStatusToServer(String message, int id, boolean forceSuccess, final int sleepSeconds) {
+        new Thread(new Runnable() {
+            public void run() {
+                HttpURLConnection httpcon;
+                String url = null;
+                String data = null;
+                String result = null;
+                try {
+                    httpcon = (HttpURLConnection) ((new URL(url).openConnection()));
+                    httpcon.setDoOutput(true);
+                    httpcon.setRequestProperty("Content-Type", "application/json");
+                    httpcon.setRequestProperty("Accept", "application/json");
+                    httpcon.setRequestMethod("POST");
+                    httpcon.connect();
+
+                    OutputStream os = httpcon.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(data);
+                    writer.close();
+                    os.close();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(httpcon.getInputStream(), "UTF-8"));
+
+                    String line = null;
+                    StringBuilder sb = new StringBuilder();
+
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    br.close();
+                    result = sb.toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Thread.sleep(sleepSeconds * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private String saveToInternalStorage(Bitmap bitmapImage) {
@@ -202,6 +243,7 @@ public class MainActivity extends AppCompatActivity {
 
                     tts.speak("Yes", TextToSpeech.QUEUE_FLUSH, null, hashCode() + "");
                 } else {
+                    sendStatusToServer("Unable to init TextToSpeech!", -1, true, 0);
                     throw new RuntimeException("Failed to init TextToSpeech!");
                 }
             }
