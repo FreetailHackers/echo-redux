bdc7aeda8316d2d0e926b8f5f86ddcac63f95d57
Raymond Chee <rjchee@gmail.com>
2017-06-24 19:22:50 -0700

    Add Heroku details

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 var http = require('http');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var httpServer = http.Server(app);
 var io = require('socket.io')(httpServer);
@@ -8,8 +9,31 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+var data = {
+    str: "",
+    complete: false
+};
+
+app.post('/start', function(req, res) {
+    console.log('start');
+    data.complete = false;
+    res.end();
+});
+
 app.post('/update', function(req, res) {
-    console.log(req.body);
+    console.log('update: ' + JSON.stringify(req.body));
+    io.sockets.emit('data', req.body);
+    if (req.body.complete) {
+        data.str = req.body.str;
+        data.complete = true;
+    }
+    res.end();
+});
+
+app.get('/end', function(req, res) {
+    res.json(data);
+    data.str = "";
+    data.complete = false;
 });
 
 app.listen(app.get('port'), function() {
